695075bda3817744ef864c99d116816811765153
vlhsmylv <iam.valeh.ismayilov@gmail.com>
2023-08-22 21:45:05 +0400

    Remove comment

--- src/components/Testimonials.tsx
+++ src/components/Testimonials.tsx
@@ -7,7 +7,6 @@ const Testimonials = () => (
     id="clients"
     className={`${styles.paddingY} ${styles.flexCenter} flex-col relative`}
   >
-    {/* TODO */}
     <div className="absolute z-[0] w-[60%] h-[60%] -right-[50%] rounded-full blue__gradient" />
 
     <div className="w-full flex justify-between items-center md:flex-row flex-col sm:mb-16 mb-6 relative z-[1]">
